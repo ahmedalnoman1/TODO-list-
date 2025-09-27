527b7291df0301769a413459e8ef341350f9de04
ahmedalnoman1 <ahmedsadikalnoman@gmail.com>
2025-09-28 00:38:55 +0300

    add DOM element

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 let currentFilter = 'all';
 let editingTaskId = null;
+
+
+// عناصر DOM
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+const taskCount = document.getElementById('taskCount');
+const clearCompletedBtn = document.getElementById('clearCompleted');
+const filterBtns = document.querySelectorAll('.filter-btn');
+const emptyState = document.getElementById('emptyState');
+const editModal = document.getElementById('editModal');
+const editTaskInput = document.getElementById('editTaskInput');
+const saveEditBtn = document.getElementById('saveEditBtn');
+const cancelEditBtn = document.getElementById('cancelEditBtn');
